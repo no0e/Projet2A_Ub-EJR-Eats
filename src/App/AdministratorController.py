905c3609387dc125116ba7d5475deafe15803420
no0e <noe.diette@eleve.ensai.fr>
2025-10-03 10:37:21 +0000

    Changement de Individual en USer

--- src/App/AdministratorController.py
+++ src/App/AdministratorController.py
@@ -1,8 +1,35 @@
 from fastapi import APIRouter, HTTPException, status
 
-from src.Model.Driver import Driver
+from src.Model.Administrator import Administrator
 
 administrator_router = APIRouter(prefix="/administrator", tags=["Administrator"])
 
 
-# @administrator_router.get("/", status_code=status.HTTP_200_OK)
+@administrator_router.post("/Create_Accounts", status_code=status.HTTP_201_CREATED)
+def Create_Accounts():
+    pass
+
+
+@administrator_router.patch("/Edit_Accounts", status_code=status.HTTP_200_OK)
+def Edit_Accounts():
+    pass
+
+
+@administrator_router.patch("/Edit_Menu", status_code=status.HTTP_200_OK)
+def Edit_Menu():
+    pass
+
+
+@administrator_router.get("/Storage/View", status_code=status.HTTP_200_OK)
+def View_Storage():
+    pass
+
+
+@administrator_router.post("/Storage/Create_Item", status_code=status.HTTP_201_CREATED)
+def Create_Item():
+    pass
+
+
+@administrator_router.patch("Storage/Replemish_Item", status_code=status.HTTP_200_OK)
+def Replemish_Item():
+    pass
--- src/App/DriverController.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, HTTPException, status
-
-from src.Model.Driver import Driver
-
-driver_router = APIRouter(prefix="/driver", tags=["Driver"])
-
-
-#@driver_router.post("/", status_code=status.HTTP_200_OK)

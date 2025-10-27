b771d49bdbd2eda6470c85623ccc00ae662a72fb
justineoliv <justine.oliviero@eleve.ensai.fr>
2025-10-27 10:35:05 +0000

    test insert_into_db from UserDAO.py

--- tests/DAO/test_UserDAO.py
+++ tests/DAO/test_UserDAO.py
@@ -39,5 +39,53 @@ def test_get_user_by_username():
     assert user.salt == "mySalt"
 
 
+class MockDBConnectorForInsert:
+    def sql_query(
+        self,
+        query: str,
+        data: dict,
+        return_type: str = "one",
+    ):
+        if "INSERT INTO users" in query:
+            return {
+                "username": data["username"],
+                "firstname": data["firstname"],
+                "lastname": data["lastname"],
+                "password": data["password"],
+                "salt": data["salt"],
+                "account_type": data["account_type"],
+            }
+        return None
+
+
+def test_insert_into_db():
+    mock_db = MockDBConnectorForInsert()
+    user_dao = UserDAO(mock_db)
+
+    username = "newuser"
+    firstname = "Alice"
+    lastname = "Asm"
+    salt = "somesalt"
+    hashed_password = "hashedpwd"
+    account_type = "Customer"
+
+    new_user = user_dao.insert_into_db(
+        username=username,
+        firstname=firstname,
+        lastname=lastname,
+        salt=salt,
+        hashed_password=hashed_password,
+        account_type=account_type,
+    )
+
+    assert new_user is not None
+    assert new_user.username == username
+    assert new_user.firstname == firstname
+    assert new_user.lastname == lastname
+    assert new_user.password == hashed_password
+    assert new_user.salt == salt
+    assert new_user.account_type == account_type
+
+
 if __name__ == "__main__":
     pytest.main()

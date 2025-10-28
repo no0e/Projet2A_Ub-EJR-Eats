2a8abf34f0574da82363e933d4e94b95d0ed6629
justineoliv <justine.oliviero@eleve.ensai.fr>
2025-10-28 14:30:20 +0000

    changed deleveydriverdao in deliverydriverdao changed item

--- src/Model/Item.py
+++ src/Model/Item.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Item(BaseModel):
-    id_item: int
+    id_item: Optional[int]
     name: str
     price: float
     category: str

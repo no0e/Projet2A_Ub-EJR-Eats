e6cae31421d09bf4cad100bd10de476180dcbd00
justineoliv <justine.oliviero@eleve.ensai.fr>
2025-11-04 15:59:44 +0000

    create table in hashtag

--- __main__.py
+++ __main__.py
@@ -1,4 +1,7 @@
 from src.App.API import run_app
+from src.DAO.DBConnector import DBConnector
 
 if __name__ == "__main__":
+    #db_connector = DBConnector()
+    #db_connector.execute_sql_file("data/init_db.sql")
     app = run_app()

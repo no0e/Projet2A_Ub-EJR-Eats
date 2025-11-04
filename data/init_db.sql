60dd2fe4a9c4eca025bff4c64b5c87985dcaa58f
no0e <noe.diette@eleve.ensai.fr>
2025-11-04 10:38:51 +0000

    fix init_db

--- data/init_db.sql
+++ data/init_db.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA project_database;
 --------------------------------------------------------------
 -- user
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.user CASCADE;
+DROP TABLE IF EXISTS project_database.users CASCADE;
 CREATE TABLE users (
     username VARCHAR PRIMARY KEY,
     firstname VARCHAR,
@@ -17,17 +17,17 @@ CREATE TABLE users (
 --------------------------------------------------------------
 -- administrator
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.administrator CASCADE;
-CREATE TABLE administrator (
+DROP TABLE IF EXISTS project_database.administrators CASCADE;
+CREATE TABLE administrators (
   username_administrator VARCHAR UNIQUE NOT NULL,
-  FOREIGN KEY (username_administrator) REFERENCES user(username)
+  FOREIGN KEY (username_administrator) REFERENCES users(username)
 );
 
 --------------------------------------------------------------
 -- delivery_driver
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.delivery_driver CASCADE;
-CREATE TABLE delivery_driver (
+DROP TABLE IF EXISTS project_database.delivery_drivers CASCADE;
+CREATE TABLE delivery_drivers (
     username_delivery_driver TEXT PRIMARY KEY REFERENCES users(username),
     vehicle TEXT,
     is_available BOOLEAN
@@ -35,31 +35,31 @@ CREATE TABLE delivery_driver (
 --------------------------------------------------------------
 -- customer
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.customer CASCADE;
-CREATE TABLE customer (
+DROP TABLE IF EXISTS project_database.customers CASCADE;
+CREATE TABLE customers (
   username_customer VARCHAR UNIQUE NOT NULL,
   address VARCHAR,
-  FOREIGN KEY (username_customer) REFERENCES user(username)
+  FOREIGN KEY (username_customer) REFERENCES users(username)
 );
 
 --------------------------------------------------------------
 -- delivery
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.delivery CASCADE;
-CREATE TABLE delivery (
+DROP TABLE IF EXISTS project_database.deliveries CASCADE;
+CREATE TABLE deliveries (
   id_delivery INTEGER UNIQUE NOT NULL PRIMARY KEY,
   username_delivery_driver VARCHAR,
   duration INTEGER,
   stops VARCHAR[],
   is_accepted BOOLEAN,
-  FOREIGN KEY (username_delivery_driver) REFERENCES user(username)
+  FOREIGN KEY (username_delivery_driver) REFERENCES users(username)
 );
 
 --------------------------------------------------------------
 -- item
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.item CASCADE;
-CREATE TABLE item (
+DROP TABLE IF EXISTS project_database.items CASCADE;
+CREATE TABLE items (
   id_item INTEGER UNIQUE NOT NULL PRIMARY KEY,
   name_item VARCHAR,
   price FLOAT,
@@ -71,15 +71,15 @@ CREATE TABLE item (
 --------------------------------------------------------------
 -- order_table
 --------------------------------------------------------------
-DROP TABLE IF EXISTS project_database.order_table CASCADE;
-CREATE TABLE order_table (
+DROP TABLE IF EXISTS project_database.orders CASCADE;
+CREATE TABLE orders (
   id_order INTEGER UNIQUE NOT NULL PRIMARY KEY,
   username_customer VARCHAR,
   username_delivery_driver VARCHAR,
   address VARCHAR,
-  items item[],
+  items items[],
   date_order DATE,
   time_order TIME,
-  FOREIGN KEY (username_customer) REFERENCES user(username),
-  FOREIGN KEY (username_delivery_driver) REFERENCES user(username)
+  FOREIGN KEY (username_customer) REFERENCES users(username),
+  FOREIGN KEY (username_delivery_driver) REFERENCES users(username)
 );

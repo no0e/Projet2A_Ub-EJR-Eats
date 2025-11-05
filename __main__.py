d25e68d5a3a1dd313d7a7f09baea74a9ac861b83
justineoliv <justine.oliviero@eleve.ensai.fr>
2025-11-05 14:52:18 +0000

    wo ancien try of db reset

--- __main__.py
+++ __main__.py
@@ -1,12 +1,4 @@
-import argparse
-
 from src.App.API import run_app
-from src.DAO.DBConnector import DBConnector
-
 
 if __name__ == "__main__":
-    #db_connector = DBConnector()
-    #db_connector.execute_sql_file("data/init_db.sql")
     app = run_app()
-
-

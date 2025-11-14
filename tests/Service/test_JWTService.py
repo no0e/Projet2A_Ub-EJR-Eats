13d0c7a23e6dc6a71098ee6bc2890aff1ce25184
no0e <noe.diette@eleve.ensai.fr>
2025-11-14 13:49:51 +0000

    fix test jwt service

--- tests/Service/test_JWTService.py
+++ tests/Service/test_JWTService.py
@@ -1,6 +1,9 @@
 import datetime
 
+import jwt
 from freezegun import freeze_time
+from jwt.exceptions import ExpiredSignatureError
+from pytest import raises
 
 from src.Service.JWTService import JwtService
 
@@ -9,19 +12,61 @@ jwt_service = JwtService("mysecret")
 
 @freeze_time("2024-08-26 12:00:00")
 def test_encode_jwt():
-    username = "myUser"
-    jwtResponse = jwt_service.encode_jwt(username=username, account_type="customer")
-    assert (
-        jwtResponse.access_token
-        == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibXlVc2VyIiwiZXhwaXJ5X3RpbWVzdGFtcCI6MTcyNDY3NDIwMC4wfQ.eUjNbpMCDNuPESsMHF2dpeRlDl6fMJmjUWsHSZT_n1Q"  # noqa: E501
-    )
+    username = "Noé"
+    account_type = "customer"
+
+    jwtResponse = jwt_service.encode_jwt(username=username, account_type=account_type)
+
+    expected_payload = {
+        "username": "Noé",
+        "account_type": "customer",
+        "expiry_timestamp": 1724674200.0,  # 12:00:00 + 600s
+    }
+
+    decoded = jwt.decode(jwtResponse.access_token, "mysecret", algorithms=["HS256"])
+    assert decoded == expected_payload
 
 
 @freeze_time("2024-08-26 12:00:00")
 def test_decode_jwt():
-    jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibXlVc2VyIiwiZXhwaXJ5X3RpbWVzdGFtcCI6MTcyNDY3NDIwMC4wfQ.eUjNbpMCDNuPESsMHF2dpeRlDl6fMJmjUWsHSZT_n1Q"  # noqa: E501
-    decoded_jwt = jwt_service.decode_jwt(jwt)
-    assert decoded_jwt.get("user_id") == "myUser"
-    assert datetime.datetime.fromtimestamp(decoded_jwt.get("expiry_timestamp")) == datetime.datetime.fromisoformat(
-        "2024-08-26 12:10:00"
-    )
+    payload = {
+        "username": "Noé",
+        "account_type": "customer",
+        "expiry_timestamp": 1724674200.0,
+    }
+
+    token = jwt.encode(payload, "mysecret", algorithm="HS256")
+
+    decoded_jwt = jwt_service.decode_jwt(token)
+
+    assert decoded_jwt["username"] == "Noé"
+    assert decoded_jwt["account_type"] == "customer"
+    assert datetime.datetime.fromtimestamp(decoded_jwt["expiry_timestamp"]) == datetime.datetime(2024, 8, 26, 12, 10, 0)
+
+
+@freeze_time("2024-08-26 12:00:00")
+def test_validate_user_jwt_valid():
+    payload = {
+        "username": "Noé",
+        "account_type": "customer",
+        "expiry_timestamp": 1724674200.0,
+    }
+
+    token = jwt.encode(payload, "mysecret", algorithm="HS256")
+
+    decoded = jwt_service.validate_user_jwt(token)
+    assert decoded == payload
+
+
+@freeze_time("2024-08-26 12:11:00")
+def test_validate_user_jwt_expired():
+    payload = {
+        "username": "Noé",
+        "account_type": "customer",
+        "expiry_timestamp": 1724674200.0,  # expire 12:10
+    }
+
+    token = jwt.encode(payload, "mysecret", algorithm="HS256")
+
+    with raises(ExpiredSignatureError):
+        jwt_service.validate_user_jwt(token)

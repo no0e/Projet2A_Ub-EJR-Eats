432c338694cc6562d6066746ef389ce26173710c
justineoliv <justine.oliviero@eleve.ensai.fr>
2025-11-15 10:24:34 +0000

    added test

--- tests/DAO/test_UserDAO.py
+++ tests/DAO/test_UserDAO.py
@@ -18,11 +18,30 @@ def user_dao(db_connector):
 def test_get_by_username(user_dao):
     ResetDatabase().lancer(True)
     user_alice = user_dao.get_by_username("aliceasm")
+    missing_user = user_dao.get_by_username("missing user")
     assert user_alice is not None
     assert user_alice.username == "aliceasm"
     assert user_alice.firstname == "Alice"
     assert user_alice.lastname == "Asm"
     assert user_alice.account_type == "Administrator"
+    assert missing_user is None
+
+def test_create_user(user_dao):
+    ResetDatabase().lancer(True)
+    to_be_created_user = User(
+        username = "created_user",
+        firstname = "Created",
+        lastname = "User",
+        password = "pwdOfCreatedUser123",
+        salt = "saltOfCreatedUser",
+        account_type = "Customer"
+    )
+    created_user = user_dao.create_user(to_be_created_user)
+    not_to_be_created_user = None
+    not_created_user = user_dao.create_user(not_to_be_created_user)
+    assert created_user == True
+    assert not_created_user == False
+
 
 
 if __name__ == "__main__":

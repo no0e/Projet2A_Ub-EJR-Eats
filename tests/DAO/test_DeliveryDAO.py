67c91ac509c78df51f3efc984f373510c23cff98
tristalbot <tristan.talbot@eleve.ensai.fr>
2025-11-20 17:10:28 +0000

    again

--- tests/DAO/test_DeliveryDAO.py
+++ tests/DAO/test_DeliveryDAO.py
@@ -13,6 +13,7 @@ def db_connector():
     db = DBConnector()
     yield db
 
+
 @pytest.fixture
 def delivery_dao(db_connector):
     return DeliveryDAO(db_connector, test=True)
@@ -23,43 +24,45 @@ def test_create(delivery_dao):
     created_delivery = Delivery(
         username_delivery_driver="ernesto",
         duration=40,
-        id_order = [3,4],
-        stops = ['13 Main St.', '4 Salty Spring Av.'],
-        is_accepted = False
+        id_order=[3, 4],
+        stops=["13 Main St.", "4 Salty Spring Av."],
+        is_accepted=False,
     )
     creation = delivery_dao.create(created_delivery)
     assert creation is True
     with pytest.raises(TypeError):
         delivery_dao.create(1)
 
+
 def test_get_available_deliveries(delivery_dao):
     ResetDatabase().lancer(True)
     available_deliveries = delivery_dao.get_available_deliveries()
     assert len(available_deliveries) == 1
+
 
 def test_get_by_id(delivery_dao):
     ResetDatabase().lancer(True)
     delivery = delivery_dao.get_by_id(1)
     assert delivery.username_delivery_driver == "ernesto"
     assert delivery.duration == 50
-    assert delivery.id_orders == [1,2]
-    assert delivery.stops == ['13 Main St.', '4 Salty Spring Av.']
+    assert delivery.id_orders == [1, 2]
+    assert delivery.stops == ["13 Main St.", "4 Salty Spring Av."]
     assert delivery.is_accepted is True
     with pytest.raises(TypeError):
         delivery_dao.get_by_id("1")
 
+
 def test_set_delivery_accepted(delivery_dao):
     id_delivery = 2
     username_delivery_driver = "ernesto1"
-    duration = 15
     update = delivery_dao.set_delivery_accepted(
         id_delivery=id_delivery,
         username_delivery_driver=username_delivery_driver,
-        duration=duration
     )
     assert update is None
     updated_delivery = delivery_dao.get_by_id(2)
     assert updated_delivery.is_accepted is True
+
 
 if __name__ == "__main__":
     pytest.main()

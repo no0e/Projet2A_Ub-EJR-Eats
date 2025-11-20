7c3f5b7c2d27c86fb163a96fb3bb209ff6dd061e
tristalbot <tristan.talbot@eleve.ensai.fr>
2025-11-20 10:26:54 +0000

    AdminController doc + cleaned

--- src/App/AdministratorController.py
+++ src/App/AdministratorController.py
@@ -1,28 +1,27 @@
-from typing import Annotated, Literal, Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Annotated, Literal, Optional
 
 from fastapi import APIRouter, Depends, HTTPException, Query, status
-from fastapi.security import HTTPAuthorizationCredentials
 
 from src.App.Auth_utils import get_user_from_credentials, require_account_type
 from src.App.JWTBearer import JWTBearer
-from src.DAO.DeliveryDAO import DeliveryDAO
-from src.Model.Administrator import Administrator
 from src.Model.APIUser import APIUser
-from src.Model.Item import Item, ItemCreate
-from src.Model.User import User
 from src.Service.PasswordService import check_password_strength
+
+if TYPE_CHECKING:
+    from fastapi.security import HTTPAuthorizationCredentials
+
+    from src.Model.Item import Item
+    from src.Model.User import User
 
 from .init_app import (
     admin_service,
     customer_repo,
-    customer_service,
-    db_connector,
     driver_repo,
-    driver_service,
     google_map_service,
     item_repo,
     item_service,
-    jwt_service,
     user_repo,
     user_service,
 )
@@ -43,23 +42,61 @@ def Create_Accounts(
     lastname: str,
     account_type: Literal["Administrator", "DeliveryDriver", "Customer"],
 ) -> APIUser:
-    """Create an account of a customer,an administrator or a delivery driver"""
+    """Function that calls the User creation function from an administrator point of view.
+
+    Parameters
+    ----------
+    username : str
+        user's username
+    password : str
+        user's password
+    firstname : str
+        user's firstname
+    lastname : str
+        user's lastname
+    account_type : str
+        user's account type
+
+
+    Returns
+    -------
+    APIUser
+        Returns the APIUser that has been created.
+    """
     try:
         check_password_strength(password=password)
-    except Exception:
-        raise HTTPException(status_code=400, detail="Password too weak")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Password too weak") from e
     try:
         user: User = admin_service.create_user(
             username=username, password=password, firstname=firstname, lastname=lastname, account_type=account_type
         )
     except Exception as error:
-        raise HTTPException(status_code=409, detail=f"{error}")
+        raise HTTPException(status_code=409, detail=f"{error}") from error
     return APIUser(username=user.username, account_type=account_type)
 
 
 @administrator_router.patch("/Edit_Accounts", status_code=status.HTTP_200_OK)
-def Edit_Accounts(username: str, attribute: Literal["firstname", "lastname", "address", "vehicle"], new_value: str):
-    """Edit the accounts of other users"""
+def Edit_Accounts(
+    username: str, attribute: Literal["firstname", "lastname", "address", "vehicle"], new_value: str
+) -> str:
+    """Function that modify a user's attribute by calling the different DAO's methods.
+
+    Parameters
+    ----------
+    username : str
+        user's username
+    attribute : str
+        user's attribute to modify
+    new_value: str
+        user's new value for the attribute selected
+
+
+    Returns
+    -------
+    str
+        Returns a string that informs the user the modification has been done.
+    """
     if attribute == "address":
         if user_service.get_user(username).account_type != "Customer":
             raise ValueError("Only customers have an address.")
@@ -81,16 +118,22 @@ def Edit_Accounts(username: str, attribute: Literal["firstname", "lastname", "ad
 
 
 @administrator_router.get("/Storage/View", status_code=status.HTTP_200_OK)
-def View_Storage():
-    """See every item even the one that are not exposed"""
+def View_Storage() -> dict:
+    """Function that print the whole set of items and their quantities to the user.
+
+    Returns
+    -------
+    dict
+        Returns a dict containing an item's name associated with its quantity in storage.
+    """
     try:
         storage = item_service.view_storage()
         print("Storage fetched:", storage)
         return storage
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     except (TypeError, ValueError) as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
 
 
 @administrator_router.post("/Storage/Create_Item", status_code=status.HTTP_201_CREATED)
@@ -99,15 +142,32 @@ def Create_Item(
     price: float,
     stock: int,
     category: str = Query(..., description="Type of item", enum=["starter", "main course", "dessert", "drink"]),
-):
-    """Create an item"""
+) -> str:
+    """Function that calls the function to create an item.
+
+    Parameters
+    -------
+    name_item: str
+        item's name
+    price: float
+        item's price
+    stock: int
+        item's quantity available
+    category: str
+        item's category
+
+    Returns
+    -------
+    str
+        Returns a string that informs the user the item has been created.
+    """
     try:
         new_item = item_service.create_item(name_item, price, category, stock)
         return {"message": "Item created successfully ", "item": new_item}
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     except (TypeError, ValueError) as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
 
 
 @administrator_router.patch("/Storage/Edit_Item", status_code=status.HTTP_200_OK)
@@ -118,14 +178,35 @@ def Edit_Item(
     new_category: str = Query(None, description="Type of item", enum=["starter", "main course", "dessert", "drink"]),
     new_stock: int = None,
     availability: bool = Query(None, description="Is the item available ?", enum=[True, False]),
-):
-    """Edit an item"""
+) -> str:
+    """Function that calls the function to update an item.
+
+    Parameters
+    -------
+    name_item: str
+        item's name
+    new_name: str
+        item's new name, by default None
+    new_price: float
+        item's new price, by default None
+    new_category: str
+        item's new category, by default None
+    new_stock: int
+        item's new quantity, by default None
+    availability: bool
+        item's availability, by default None
+
+    Returns
+    -------
+    str
+        Returns a string that informs the user the item has been updated.
+    """
     try:
         item_service.update(name_item, new_name, new_price, new_category, new_stock, availability)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     except (TypeError, ValueError) as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     if new_name is None:
         item = item_repo.find_item_by_name(name_item)
     else:
@@ -141,15 +222,26 @@ def Edit_Item(
 
 
 @administrator_router.delete("/Storage/Delete_Item", status_code=status.HTTP_200_OK)
-def Delete_Item(name_item):
-    """Delete an item"""
+def Delete_Item(name_item) -> Item:
+    """Function that calls the function to delete an item.
+
+    Parameters
+    -------
+    name_item: str
+        item's name
+
+    Returns
+    -------
+    Item
+        Returns the Item that has been deleted.
+    """
     try:
         item_deleted = item_service.delete_item(name_item)
         return item_deleted
     except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
     except (TypeError, ValueError) as e:
-        raise HTTPException(status_code=400, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e)) from e
 
 
 @administrator_router.patch("/Storage/Edit_Profile", status_code=status.HTTP_200_OK)
@@ -159,14 +251,31 @@ def edit_Profile(
     password: Optional[str] = Query(None, description="Password"),
     credentials: Annotated[HTTPAuthorizationCredentials, Depends(JWTBearer())] = None,
 ):
-    """Edit the attributes of the connected administrator."""
+    """Function that calls the function to update the user's own profile
+
+    Parameters
+    -------
+    firstname: str
+        admin's new firstname, by default None
+    lastname: str
+        admin's new lastname, by default None
+    password: str
+        admin's new password, by default None
+    credentials: HTTPAuthorizationCredentials
+        admin's credentials, by default None
+
+    Returns
+    -------
+    Item
+        Returns the Item that has been deleted.
+    """
 
     username = get_user_from_credentials(credentials).username
 
     try:
         user_service.update_user(username, firstname, lastname, password)
     except Exception as error:
-        raise HTTPException(status_code=403, detail=f"Error updating profile: {error}")
+        raise HTTPException(status_code=403, detail=f"Error updating profile: {error}") from error
 
     return {
         "detail": "Profile updated successfully",
